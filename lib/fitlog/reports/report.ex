636f4b0e183f690b9c20dcf92fa84ec5f94899e2
Sergey Zubkov <ksevelyar@protonmail.com>
2022-07-13 00:26:25 +0300

    add ex_machina and create report's factory

--- lib/fitlog/reports/report.ex
+++ lib/fitlog/reports/report.ex
@@ -28,8 +28,8 @@ defmodule Fitlog.Reports.Report do
     from reports in query, where: reports.user_id == ^user_id
   end
 
-  def list_user_reports(user) do
-    from(r in Report, order_by: [desc: r.date])
+  def list_user_reports(user, limit) do
+    from(r in Report, order_by: [desc: r.date], limit: ^limit)
     |> Bodyguard.scope(user)
     |> Repo.all()
   end
